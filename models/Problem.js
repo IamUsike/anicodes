5c9814a96f5e5c1e58b2c299a7e6c53fe2b71d29
IamUsike <sathwikthanmay4@gmail.com>
2025-05-17 10:29:32 +0530

    export issue

--- models/Problem.js
+++ models/Problem.js
@@ -1,40 +1,45 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const ProblemSchema = Schema({
-    id: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    title: {type: String},
-    problemStatement: {type: String},
-    inputFormat: {type: String},
-    outputFormat: {type: String},
-    sampleInput: {type: String},
-    sampleOutput: {type: String},
-    likes: {
-        type: Number,
-        default: 10
-    },
-    dislikes: {
-        type: Number,
-        default: 0
-    },
-    order: {type: Number},
-    category: {type: String},
-    constraints: {type: String},
-    companies: {type: [String], default: []},
-    starterCode: {type: String},
-    difficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], default: 'Easy'},
-    // totalpoints: { type: Number, default:0 ,select:false },
-    points: { type:Number, enum:['1','2','3'],default:1},
-    solution: {type: String},
-    videoId: {type: String},
-    testCases: [{
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: { type: String },
+  problemStatement: { type: String },
+  inputFormat: { type: String },
+  outputFormat: { type: String },
+  sampleInput: { type: String },
+  sampleOutput: { type: String },
+  likes: {
+    type: Number,
+    default: 10,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+  order: { type: Number },
+  category: { type: String },
+  constraints: { type: String },
+  companies: { type: [String], default: [] },
+  starterCode: { type: String },
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+    default: "Easy",
+  },
+  // totalpoints: { type: Number, default:0 ,select:false },
+  points: { type: Number, enum: ["10", "20", "30"], default: 1 },
+  solution: { type: String },
+  videoId: { type: String },
+  testCases: [
+    {
       input: { type: [String], required: true },
       output: { type: [String], required: true },
-    }],
-  });
-  
-export const Problem = models?.Problem || model('Problem', ProblemSchema);
-export const UserPoints = models?.UserPoints || model('UserPoints',UserPointsSchema)
+    },
+  ],
+});
+
+export const Problem = models?.Problem || model("Problem", ProblemSchema);
